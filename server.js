61aa91f4666db31f2e72506295754fae46ece1fe
Zidor200 <ouchikh9one@gmail.com>
2025-04-22 02:29:10 +0200

    added a debug for request test

--- server.js
+++ server.js
@@ -38,6 +38,11 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Chanitec API' });
 });
 
+app.use((req, res, next) => {
+    console.log('Received request:', req.originalUrl);
+    next();
+  });
+
 // API info route
 app.get('/api', (req, res) => {
     res.json({
